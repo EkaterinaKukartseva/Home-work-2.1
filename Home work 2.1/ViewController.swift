6922ef7708c313d3c3efe60eb712a956e211cd98
Ekaterina Kukartseva <sos91195@mail.ru>
2020-12-02 15:07:56 +0300

    Initial Commit

---
+++ Home work 2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home work 2.1
+//
+//  Created by EKATERINA  KUKARTSEVA on 02.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
